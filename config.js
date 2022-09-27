250d402ccf4bf258128e6b85421491894e758bf5
Alfonso FUCA <afuca@environnement.brussels>
2022-09-27 11:11:33 +0200

    Config: desktop emulation

--- config.js
+++ config.js
@@ -1,10 +1,21 @@
 module.exports = {
     extends: 'lighthouse:default',
 
+    settings: {
+        formFactor: 'desktop',
+        screenEmulation: {
+            mobile: false,
+            width: 1350,
+            height: 940,
+            deviceScaleFactor: 1,
+            disabled: false,
+        }
+    },
+
     passes: [{
         passName: 'defaultPass',
         gatherers: [
             'gatherers/myGatherer',
         ],
     }],
-  };
+};
